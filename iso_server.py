f7d7091afa71c6d2240d5acda38cde43157b8817
Junior_FOX <cjuniorfox@gmail.com>
2024-06-03 10:09:00 -0300

    download uses 7zip

--- iso_server.py
+++ iso_server.py
@@ -10,6 +10,15 @@ CONTEXT = os.environ.get('CONTEXT')
 CONTEXT = CONTEXT if CONTEXT != None else '/ipxe/isos/'
 ISO_DIR = ISO_DIR if ISO_DIR != None else '/home/junior/Downloads/isos'
 
+def to_dict(path,name,kind):
+    if kind not in ['FILE','DIR','BACK','ISO']:
+        raise ValueError(f'Kind must be FILE, DIR or UP, but is {kind}')
+    return {
+        'path': path,
+        'name': name,
+        'kind': kind
+    }
+
 def to_html(iso_name, files, file_path):
     folder = file_path.replace('path:', '')
     head = f'''<html><head><title>Index of {folder}</title><style>body {{ font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f4f4f4; }} .container {{ width: 80%; margin: auto; overflow: hidden; }} header {{ background: #333; color: #fff; padding-top: 30px; min-height: 70px; border-bottom: #77aaff 3px solid; }} header a {{ color: #fff; text-decoration: none; text-transform: uppercase; font-size: 16px; }} ul {{ list-style: none; padding: 0; }} ul li {{ background: #fff; margin: 5px 0; padding: 10px; border: #ccc 1px solid; }} ul li a {{ color: #333; text-decoration: none; }} ul li a:hover {{ color: #77aaff; }} hr {{ border: 0; height: 1px; background: #ccc; margin: 20px 0; }}</style></head>'''
@@ -23,7 +32,7 @@ def to_html(iso_name, files, file_path):
             kind = item['kind']
             fa_icon = 'fa-folder' if kind=='DIR' else 'fa-compact-disc'
             fa_icon = 'fa-file' if kind=='FILE' else fa_icon
-            fa_icon = 'fa-turn-up' if kind=='UP' else fa_icon
+            fa_icon = 'fa-turn-up' if kind=='BACK' else fa_icon
             body += f'<li><a href="{path}"><i class="fas {fa_icon} icon"></i> {name}</a></li>'
     bottom = '''</ul><hr /></div></body></html>'''
     return head + body + bottom
@@ -33,11 +42,7 @@ def list_isos_as_dict():
     try:
         for f in os.listdir(ISO_DIR):
             if f.endswith('.iso'):
-                files.append({
-                    'path':f'''{CONTEXT}html/{f.removesuffix('.iso')}''',
-                    'name':f,
-                    'kind':'ISO'
-                })
+                files.append(to_dict(f'''{CONTEXT}html/{f.removesuffix('.iso')}''',f,'ISO'))
         return files
     except Exception as e:
         abort(500, description=f"Error listing ISO files: {str(e)}")
@@ -46,58 +51,69 @@ def list_iso_contents_dict(iso_name, path='/'):
     iso_path = os.path.join(ISO_DIR, iso_name + ".iso")
     if not os.path.isfile(iso_path):
         abort(404, description="ISO file not found")
+    
     try:
-        files=[]
-        cmd = ['7z','l',iso_path]
+        files = []
+        cmd = ['7z', 'l', iso_path]
         iso = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        output= iso.stdout.split('\n')
+        output = iso.stdout.split('\n')
+        
         path = path.replace("path:", "")
         last_dir = ''
-        files_buffer=False
-        if path not in ['','/']:
-            files.append({
-                'path': os.path.join(CONTEXT, 'html',iso_name) + '/path:' + path+'..',
-                'name': 'Up one directory',
-                'kind':'UP'
-            })
-        else :
-            files.append({
-                'path': os.path.join(CONTEXT, 'html'),
-                'name': 'ISO Directory Listing',
-                'kind':'UP'
-            })
-        for child in output:
-            if child.startswith('------'):
-                files_buffer=True
-            values=child.split()
-            if len(values) == 6 and files_buffer:
-                full_path=os.path.join('/',values[5])
-                name=os.path.basename(values[5])
-                dir_name = os.path.dirname(full_path)
-                path_look = os.path.join('/',path)
-                len_look_dir=len(path_look.split('/'))-1
-                #add dirs
-                if dir_name.startswith(path_look):
-                    split_dir=dir_name.split('/')
-                    if(len(split_dir) > len_look_dir ):
-                        dir_name_split = dir_name.split('/')
-                        subdir=dir_name_split[len_look_dir]
-                        if subdir != last_dir and subdir != '' and subdir != None:
-                            last_dir = subdir
-                            files.append({
-                                'path': os.path.join(CONTEXT, 'html',iso_name) + '/path:' + os.path.join(path_look,subdir+'/'),
-                                'name': subdir,
-                                'kind':'DIR'
-                            })
-
-                #add files    
-                if dir_name == path_look or dir_name == path_look.removesuffix('/'):
-                    files.append({
-                            'path': os.path.join(CONTEXT, 'download', iso_name) + '/path:' + full_path,
-                            'name': name,
-                            'kind':'FILE'
-                        })
+        files_buffer = False
+        
+        # Add the "Up one directory" link
+        if path not in ['', '/']:
+            files.append(to_dict(
+                os.path.join(CONTEXT, 'html', iso_name) + '/path:' + path + '..',
+                'Up one directory',
+                'BACK'
+            ))
+        else:
+            files.append(to_dict(
+                os.path.join(CONTEXT, 'html'),
+                'ISO Directory Listing',
+                'BACK'
+            ))
+        
+        for line in output:
+            if line.startswith('------'):
+                files_buffer = True
+                continue
+            
+            if files_buffer:
+                values = line.split()
+                if len(values) == 6:
+                    full_path = os.path.join('/', values[5])
+                    name = os.path.basename(values[5])
+                    dir_name = os.path.dirname(full_path)
+                    path_look = os.path.join('/', path)
+                    len_look_dir = len(path_look.split('/')) - 1
+                    
+                    # Add directories
+                    if dir_name.startswith(path_look):
+                        split_dir = dir_name.split('/')
+                        if len(split_dir) > len_look_dir:
+                            subdir = split_dir[len_look_dir]
+                            if subdir and subdir != last_dir:
+                                last_dir = subdir
+                                files.append(to_dict(
+                                    os.path.join(CONTEXT, 'html', iso_name) + '/path:' + os.path.join(path_look, subdir + '/'),
+                                    subdir,
+                                    'DIR'
+                                ))
+                    
+                    # Add files
+                    if dir_name == path_look or dir_name == path_look.rstrip('/'):
+                        files.append(to_dict(
+                            os.path.join(CONTEXT, 'download', iso_name) + '/path:' + full_path,
+                            name,
+                            'FILE'
+                        ))
+        files = sorted(files, key=lambda x: x['kind'])
         return files
+    except subprocess.CalledProcessError as e:
+        abort(500, description=f"Error running 7z command: {str(e)}")
     except Exception as e:
         abort(500, description=f"Unexpected error: {str(e)}")
 
@@ -124,23 +140,17 @@ def download_file(iso_name, file_path):
     if not os.path.isfile(iso_path):
         abort(404, description="ISO file not found")
     try:
-        file_path = file_path.replace('path:', '').upper()
-        iso = pycdlib.PyCdlib()
-        iso.open(iso_path)
+        file_path = file_path.replace('path:', '').lstrip('/')
         extracted_path = os.path.join("/tmp", iso_name, os.path.basename(file_path))
         extracted_dir = os.path.dirname(extracted_path)
         os.makedirs(extracted_dir, exist_ok=True)
-        with open(extracted_path, 'wb') as f:
-            try:
-                iso.get_file_from_iso_fp(f, iso_path=file_path.upper() + ";1")
-            except pycdlib.pycdlibexception.PyCdlibException:
-                iso.get_file_from_iso_fp(f, iso_path=file_path.upper())
-        iso.close()
+        cmd=['7z', 'e', iso_path, f'-o{extracted_dir}' , file_path ]
+        result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if result.returncode != 0:
+            abort(500, description=f"Error extracting file: {result.stderr}")
         return send_file(extracted_path, as_attachment=True)
-    except pycdlib.pycdlibexception.PyCdlibException as e:
-        abort(500, description=f"Error extracting file: {str(e)}")
     except Exception as e:
-        abort(500, description=f"Unexpected error: {str(e)}")
+        abort(500, description=f"Unexpected error: {str(e)} str{result.stdout}")
 
 @app.route(f'{CONTEXT}json/')
 def list_isos():
